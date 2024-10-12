1aae7eecac69c5ac99ebfcf1cfc28296ea80b829
Muhammad272879 <93262239+Muhammad272879@users.noreply.github.com>
2024-10-13 00:42:37 +0800

    Add files via upload

---
+++ Controllers/YouTubeController.cs.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YouTubeApiProject.Models;
+using YouTubeApiProject.Services;
+
+namespace YouTubeApiProject.Controllers
+{
+    public class YouTubeController : Controller
+    {
+        private readonly YouTubeApiService _youTubeApiService;
+
+        public YouTubeController(YouTubeApiService youTubeApiService)
+        {
+            _youTubeApiService = youTubeApiService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return View(new List<YouTubeVideoModel>());
+            }
+
+            var videos = await _youTubeApiService.SearchVideosAsync(query);
+            return View("Index",videos);
+        }
+    }
+}
